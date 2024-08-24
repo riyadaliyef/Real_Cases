698322dbdec86dc23e72070354f5d5d3174802e2
riyadaliyef <riyaddaliyev@gmail.com>
2024-08-24 23:53:49 +0400

    new case

---
+++ Accident Analysis for Insurance Company/insurancecom.sql
@@ -0,0 +1,19 @@
+#task 1
+SELECT 
+    TYPEOFACCIDENT, 
+    Dayname(str_to_date(DATE, '%d.%m.%Y')) as Weekdays , # txt oldugu ucun date cevirdim 
+    Count(*) as countofaccidents
+FROM tasks.insurancecompany
+group by TYPEOFACCIDENT, Weekdays
+#order by TYPEOFACCIDENT
+
+;
+#task 2
+
+SELECT 
+	REGIONNAME,
+	avg(cast(replace(SPEED, 'km/hr', '') as signed integer)) as averagespeed
+FROM tasks.insurancecompany
+where REGIONNAME like '%NORTH%' or REGIONNAME like '%SOUTH%'
+group by REGIONNAME;
+
